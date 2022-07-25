aad5890c84d1de6e126bb2c7a830643091ced871
karingi <emmanuelkaringi@gmail.com>
2022-07-25 11:11:14 +0300

    Made changes to conform with PEP8 standards

--- app.py
+++ app.py
@@ -3,20 +3,20 @@
 import sys
 
 #Declare the vowels
-vowels = 'aeiou'
+VOWELS = 'aeiou'
 
 while True:
     #Ask user to input a word
     word = input("Type a word to see it's Pig Latin translation: ")
 
     #Check if first letter is a vowel then add 'way' at the end of the word.
-    if word[0] in vowels:
+    if word[0] in VOWELS:
         pig_latin = word+'way'
     #If first letter is not a vowel, move it at the end then add 'ay'
     else:
         pig_latin = word[1:] + word[0] +'ay'
     print()
-    print("{}".format(pig_latin))
+    print(pig_latin)
 
     #Ask the user to try again or quit.
     try_again = input("\n\nTry again?(Press Enter, else, n to stop)\n")
